15f466db86c63332a9e922ef27b8ebb578bb70ca
Naveen56001 <np822590@gmail.com>
2024-08-15 00:30:00 +0530

    tailwindcss added

--- src/App.js
+++ src/App.js
@@ -12,27 +12,27 @@ function App() {
   }, [weight, height]);
 
   return (
-    <main>
-      <h1>BMI Calculator</h1>
-      <div className="input-section">
-        <p className="slider-output">Weight: {weight} kg</p>
-        <input className='input-slider'
+    <main className="flex flex-col items-center text-center bg-white font-sans w-[90vw] max-w-[500px] mx-auto mt-12 mb-5">
+      <h1 className="bg-[#1b1096] text-white w-full mb-3 text-4xl px-5 py-3">BMI Calculator</h1>
+      <div className="w-[70%]">
+        <p>Weight: {weight} kg</p>
+        <input className="w-full mt-1"
         type='range'
         step='1'
         min='40'
         max='200' 
         onChange={(e) => setWeight(e.target.value)}/>
-        <p className="slider-output">Height: {height} cm</p>
-        <input className='input-slider'
+        <p>Height: {height} cm</p>
+        <input className="w-full mt-1"
         type='range'
         step='1'
         min='80'
         max='300'
         onChange={(e) => setHeight(e.target.value)}/>
       </div>
-      <div className="output-section">
+      <div>
         <p>Your BMI is: </p>
-        <p className='output'>
+        <p className="px-5 py-3 rounded-full bg-[#1b1096] mt-3 text-[#fff] mb-1">
           {output}
         </p>
       </div>
